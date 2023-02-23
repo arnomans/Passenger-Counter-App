6d4068328bc3624651c8a2479a431903d14eb081
arnomans <arnomans@outlook.com>
2023-02-23 20:18:04 +0200

    Add Save button

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ let count = 0;
 let countEl = document.getElementById("count-el");
 
 function increment() {
-    count = count +1;
+    count = count + 1;
     countEl.innerHTML = count;
 }
+
+function save() {
+    console.log(count);
+}
+
+let username = "Arno";
+let message = " You have three new notifications";
+
+console.log(username + message);
+
+let messageToUser = username + message;
+
+console.log(messageToUser);
+
+
+
+let name = "Arno";
+let greeting = "Hi, my name is ";
+let myGreeting = greeting + name;
+
+console.log(myGreeting);
